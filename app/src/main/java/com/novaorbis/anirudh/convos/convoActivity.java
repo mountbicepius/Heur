029aa7677278ed1786c312d72e184c301bb3329e
mountbicepius <mountbicepius@github.com>
2018-03-21 20:52:52 +0530

    did some stuff

--- app/src/main/java/com/novaorbis/anirudh/convos/convoActivity.java
+++ app/src/main/java/com/novaorbis/anirudh/convos/convoActivity.java
@@ -1,15 +1,15 @@
 package com.novaorbis.anirudh.convos;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,139 +17,82 @@ import java.util.List;
 
 public class convoActivity extends Activity {
 
+    private RecyclerView mMessageCache;
+    private EditText mSendText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convo);
-        RecyclerView chats = findViewById(R.id.convos);
-        chats.setHasFixedSize(true);
-        chats.setLayoutManager(new LinearLayoutManager(this));
-        EditText sent = findViewById(R.id.msgTxt);
-        TextView recieved = findViewById(R.id.recieved);
-        List<chats> chtLst= new ArrayList<chats>();
-         ImageButton btn = findViewById(R.id.sndBtn);
-         btn.setOnClickListener(v -> {
-             chtLst.add(new chats(sent.getText().toString(),recieved.getText().toString()));
-                chats.setAdapter(new ChatAdapter(chtLst,this));
-         });
+        convosMsg message =new convosMsg();
+        mMessageCache = findViewById(R.id.messageRecyclerView);
+        Button mSend = findViewById(R.id.sendButton);
+        mSendText = findViewById(R.id.messageEditText);
+        LinearLayoutManager obj = new LinearLayoutManager(this);
+        obj.setStackFromEnd(true);
+        mMessageCache.setLayoutManager(obj);
+        mMessageCache.setAdapter(new convoAdapter(getMessages()));
+        mSend.setOnClickListener(v -> {
+            message.Msg = mSendText.getText().toString();
+            getMessages().add(message);
+
+            mSendText.setText("");
+        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    public List<convosMsg> getMessages() {
+        return new ArrayList<convosMsg>();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-    private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
-        TextView messageText;
-        ReceivedMessageHolder(View itemView) {
-            super(itemView);
-            messageText = itemView.findViewById(R.id.recieved);
-        }
-
-        void bind(chats message) {
-            messageText.setText(message.recd);
-
-
-            // Insert the profile image from the URL into the ImageView.
-        }
-    }
-    private class SentMessageHolder extends RecyclerView.ViewHolder {
-        TextView messageText;
-        SentMessageHolder(View itemView) {
-            super(itemView);
-            messageText = itemView.findViewById(R.id.recieved);
-        }
-
-        void bind(chats message) {
-            messageText.setText(message.sent);
-
-
-            // Insert the profile image from the URL into the ImageView.
-        }
-    }
-    public class chats
+    public  class convosMsg
     {
-        String sent;
-        String recd;
-        public chats(String sent, String recd)
+        String Msg;
+        long timestamp;
+        public convosMsg ()
         {
-            this.sent = sent;
-            this.recd = recd;
+
+        }
+        public convosMsg (String msg,long timestamp)
+        {
+            this.Msg =msg;
+            this.timestamp = timestamp;
         }
     }
 
 
-
-    private class ChatAdapter extends RecyclerView.Adapter{
-
-        private static final int VIEW_TYPE_MESSAGE_SENT = 1;
-        private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
-
-
-        private List<chats> msgs;
-        String sent;
-        String recd;
-        private Context ctxt;
-        public ChatAdapter(List<chats> chatsList, Context ctxt)
+    public class convoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+    {
+        List<convosMsg> convosMsgList;
+        public convoAdapter(List<convosMsg> msgList)
         {
-            this.msgs = chatsList;
-            this.ctxt =ctxt;
+            this.convosMsgList = msgList;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View layoutInflater = LayoutInflater.from(parent.getContext()).inflate(R.layout.bubble2,parent,false);
+            return new recievedMsg(layoutInflater);
         }
 
         @Override
-        public int getItemViewType(int position) {
-            chats message = (chats) msgs.get(position);
-
-            if (getCallingActivity().getClassName().equals(this.getClass().getSimpleName())) {
-                // If the current user is the sender of the message
-                return VIEW_TYPE_MESSAGE_SENT;
-            } else {
-                // If some other user sent the message
-                return VIEW_TYPE_MESSAGE_RECEIVED;
-            }
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            TextView txt = (TextView) holder.itemView;
+            txt.setText(convosMsgList.get(position).toString());
         }
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view;
-            if (viewType == VIEW_TYPE_MESSAGE_SENT)
-            {
-                view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.bubble, parent, false);
-                return new SentMessageHolder(view);
-            }
-            else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
-                view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.bubble2, parent, false);
-                return new ReceivedMessageHolder(view);
-            }
-
-            return null;
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            chats message = (chats) msgs.get(position);
-
-            switch (holder.getItemViewType()) {
-                case VIEW_TYPE_MESSAGE_SENT:
-                    ((SentMessageHolder) holder).bind(message);
-                    break;
-                case VIEW_TYPE_MESSAGE_RECEIVED:
-                    ((ReceivedMessageHolder) holder).bind(message);
-            }
-        }
-
 
         @Override
         public int getItemCount() {
-            return msgs.size();
+            return convosMsgList.size();
         }
     }
+    public class recievedMsg extends RecyclerView.ViewHolder
+    {
+        TextView msg;
 
+        public recievedMsg(View itemView) {
+            super(itemView);
+            msg = itemView.findViewById(R.id.recieved);
+        }
+    }
 }
